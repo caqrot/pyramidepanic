26cdd957de59b0817476256c51523c587d79750b
kjell de weijer <caqrot@hotmail.com>
2013-12-12 10:05:01 +0100

    Voor startscene image toegevoegd

--- PyramidPanicTheGame/PyramidPanicTheGame/HelperClasses/Image.cs
+++ PyramidPanicTheGame/PyramidPanicTheGame/HelperClasses/Image.cs
@@ -1,11 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace PyramidPanic.HelperClasses
 {
-    class Image
+    public class Image
     {
+        
+        #region Fields
+		 /* Fields zijn private en zijn alleen beschikbaar binnen de class . Ze wordenn ook wel 
+          * class variables genoemd
+          */
+
+         /* Met een Texture2D kun je een plaatje zichtbaar maken. Is eigenlijk een soort houten
+          * bord waar je een poster op kunt plakken
+          */
+        private Texture2D texture;
+
+        /* De rectangle gebruiken we voor collisiondetection
+         */
+
+	    #endregion
+
+        #region Properties
+        
+        #endregion
+
+        #region Contructor
+        /* Dit is de contructor van de Image class. Hij is meestal public, heeft
+         * dezelfde naam als de class en heeft geen returntype.
+         */
+          public Image(PyramidPanic game, string pathNameAsset, Vector2 position)
+        {
+            this.game = game;
+            this.texture = game.Content.Load<Texture2D>(pathNameAsset);
+            this.rectangle = new Rectangle((int)position.X,
+                                           (int)position.Y,
+                                            this.texture.Width,
+                                            this.texture.Height);
+                                           
+        }
+        #endregion
+
+        #region Update
+        
+        #endregion
+
+        #region Draw
+          public void Draw(GameTime gamTime);
+        {
+            this.game.
+        }
+        #endregion
+
     }
 }
