a4257b4e429f8beffcd1ace57e5c07f31af2109c
kjell de weijer <caqrot@hotmail.com>
2014-01-17 09:29:05 +0100

    Beetle en IBeetleState class gemaakt

---
+++ PyramidPanicTheGame/PyramidPanicTheGame/GameScenes/PlayScene/Beetle/Beetle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace PyramidPanic
+{
+    public class Beetle
+    {
+        // Fields
+        private Vector2 position;
+        private int speed;
+        private PyramidPanic game;
+        private IBeetleState state;
+
+        //Properties
+        public Vector2 Position
+        {
+            get { return this.position; }
+            set { this.position = value; }
+        }
+        public PyramidPanic Game
+        {
+            get { return this.game; }
+        }
+        public int Speed
+        {
+            get { return this.speed;}
+        }
+        public IBeetleState State
+        {
+            get { return this.state; }
+            set { this.state = value; }
+        }
+
+        // Constructor
+        public Beetle(PyramidPanic game, Vector2 position)
+        {
+            this.position = position;
+            this.game = game;
+        }
+
+        // Update
+        public void Update(GameTime gameTime)
+        {
+            this.state.Update(gameTime);
+        }
+
+        // Draw
+        public void Draw(GameTime gameTime)
+        {
+            this.state.Draw(gameTime);
+        }
+    }
+}
